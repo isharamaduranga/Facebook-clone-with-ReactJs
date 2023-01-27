0c408860049a4fc3f5afec769cb550b28e2b1799
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-27 21:01:53 +0530

    import and define container content component with Home Page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import NavBar from '../../Components/Navigation/NavBar'
+import LeftPane from '../../Components/Left-Pane/LeftPane'
 
 export default function Home() {
   return (
-    <div>
+    <Fragment>
         <NavBar/>
-    </div>
+        <div className='Container'>
+           <LeftPane/>
+        </div>
+       
+    </Fragment>
     
   )
 }
