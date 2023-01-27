9e113059594f60b8c454bd458dfae50f31bc1c2a
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-27 21:36:12 +0530

    content & styles updated with left pain menu..

--- src/Components/Left-Pane/LeftPane.js
+++ src/Components/Left-Pane/LeftPane.js
@@ -1,8 +1,78 @@
-import React from 'react'
-import "./leftPane.css"
+import React from 'react';
+import "./leftPane.css";
+import MarkUnreadChatAltIcon from '@mui/icons-material/MarkUnreadChatAlt';
+import GroupIcon from '@mui/icons-material/Group';
+import RssFeedIcon from '@mui/icons-material/RssFeed';
+import FlagIcon from '@mui/icons-material/Flag';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import BuildIcon from '@mui/icons-material/Build';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 export default function LeftPane() {
   return (
-    <div>LeftPane</div>
+    <div className='leftPane'>
+     <div className="leftPainContainer">
+      <div className="leftPainMenu">
+        <li className="leftPainMenuItem">
+
+          <MarkUnreadChatAltIcon className='leftPainIcons'/>
+          <span className="leftMenuText">Messages</span>
+        </li>
+
+        <li className="leftPainMenuItem">
+          <GroupIcon className='leftPainIcons'/>
+          <span className="leftMenuText">Groups</span>
+        </li>
+
+        <li className="leftPainMenuItem">
+          <RssFeedIcon className='leftPainIcons'/>
+          <span className="leftMenuText">Feed</span>
+        </li>
+
+        <li className="leftPainMenuItem">
+          <FlagIcon className='leftPainIcons'/>
+          <span className="leftMenuText">Pages</span>
+        </li>
+
+        <li className="leftPainMenuItem">
+          <CalendarMonthIcon className='leftPainIcons'/>
+          <span className="leftMenuText">Events</span>
+        </li>
+
+        <li className="leftPainMenuItem">
+          <BuildIcon className='leftPainIcons'/>
+          <span className="leftMenuText">Settings</span>
+        </li>
+
+        <li className="leftPainMenuItem">
+          <SportsEsportsIcon className='leftPainIcons'/>
+          <span className="leftMenuText">Games</span>
+        </li>
+
+        <li className="leftPainMenuItem">
+          <NewspaperIcon className='leftPainIcons'/>
+          <span className="leftMenuText">News</span>
+        </li>
+
+        <li className="leftPainMenuItem">
+          <WorkOutlineIcon className='leftPainIcons'/>
+          <span className="leftMenuText">Jobs</span>
+        </li>
+
+        <li className="leftPainMenuItem">
+          <AddShoppingCartIcon className='leftPainIcons'/>
+          <span className="leftMenuText">Market</span>
+        </li>
+
+        <hr/>
+
+        
+
+      </div>
+     </div>
+      </div>
   )
 }
