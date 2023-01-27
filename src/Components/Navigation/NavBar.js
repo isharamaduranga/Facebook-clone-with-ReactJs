cc340eb60c3f7da923ee89eee746baa48457e906
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-27 13:37:17 +0530

    content updated & import material Ui Iconss

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div>Navigation Bar</div>
+    <div className="navBarBox">
+
+        <div className="navBarLeft">
+            <span className="logo"> facebook</span>
+        </div>
+
+        <div className="navBarCenter">
+            <div className="searchBox"></div>
+        </div>
+
+        <div className="navBarRight">
+
+        </div>
+
+      
+
+    </div>
   )
 }
