d3853d933d075aeef81bdac3ef61d4eaac8a1902
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-01-28 13:48:36 +0530

    Content updated..

--- src/Components/Middle-Pane/MiddlePane.js
+++ src/Components/Middle-Pane/MiddlePane.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Display_Post from '../Display_Post/Display_Post';
+import Post from '../Post/Post';
 import "./middlePane.css";
 
 export default function MiddlePane() {
   return (
     <div className='middlePane'>
     <Display_Post/>
+    <Post/>
     </div>
 
   )
--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -3,6 +3,27 @@ import "./post.css"
 
 export default function Post() {
   return (
-    <div>Post</div>
+    <div className='post'>
+        <div className="postContainer">
+
+
+          <div className="postTop">
+              <img src="/images/postone.jpg" alt="" className="postImage" />
+              <span className="postUserName">Ishara Maduranga</span>
+              <span className='postTime'>10 mins ago...</span>
+          </div>
+
+          <div className="postCenter">
+
+          </div>
+
+          <div className="postBottom">
+
+          </div>
+
+
+        </div>
+
+    </div>
   )
 }
